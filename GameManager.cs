de376138a9e616ea6328eff44b94523a3c5ab96a
MVakharia <michaelvakharia@gmail.com>
2021-04-09 12:41:50 +0100

    Added Game Manager and added functionality to machines.

---
+++ GameManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public enum GameState
+{
+    Build, Assault
+}
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameState currentGameState;
+
+    public GameState CurrentGameState => currentGameState;
+
+    public bool IsInBuildPhase => currentGameState == GameState.Build;
+
+    public bool IsInAssaultPhase => currentGameState == GameState.Assault;
+
+    private static GameManager singleton;
+
+    public static GameManager Singleton
+    {
+        get
+        {
+            if(singleton == null)
+            {
+                singleton = GameObject.FindGameObjectWithTag("Game Manager").GetComponent<GameManager>();                    
+            }
+            return singleton;
+        }
+    }
+
+    private void GoToBuildPhase() => currentGameState = GameState.Build;
+
+    public void GoToAssaultPhase() => currentGameState = GameState.Assault;
+
+    public void Start()
+    {
+        GoToBuildPhase();
+    }
+}
--- Machine.cs
+++ Machine.cs
@@ -1,77 +1,96 @@
 ﻿using UnityEngine;
+using TMPro;
 
 public enum MachineState
 {
-    Off, On
+    Off, Standby, On
 }
 
 public abstract class Machine : MonoBehaviour
 {
+    #region Fields
     [SerializeField]
     protected MachineState powerState;
-
     [SerializeField]
     protected float temperature;
-
     [SerializeField]
     protected CoolingSystem connectedCoolingSystem;
+    [SerializeField]
+    protected TMP_Text temperatureDisplay;
+    [SerializeField]
+    protected TMP_Text powerStateDisplay;
+
 
     public const float epoTemperature = 85;
+    #endregion
 
-    protected void TurnOnMachine() => powerState = MachineState.On;
-
-    protected void TurnOffMachine() => powerState = MachineState.Off;
-
+    #region Properties
+    protected bool MachineIsOn => powerState == MachineState.On;
+    protected bool MachineIsInStandby => powerState == MachineState.Standby;
+    protected bool MachineIsOff => powerState == MachineState.Off;
     protected bool MachineAboveSafeTemperature => temperature >= epoTemperature;
+    protected bool GoneIntoStandby = false;
+    #endregion
 
-    protected void SetTemperatureToAmbient ()
-    {
-        temperature = WeatherController.Singleton.Temperature;
-    }
 
-    protected void IncreaseTemperaturePerSecond (float amount)
-    {
-        temperature += amount * Time.deltaTime;
-    }
-
-    protected void HeatUp ()
-    {
-        IncreaseTemperaturePerSecond(2);        
-    }
-
+    protected void TurnOn() => powerState = MachineState.On;
+    protected void GoIntoStandby() => powerState = MachineState.Standby;
+    protected void TurnOff() => powerState = MachineState.Off;
+    protected void SetTemperatureToAmbient() => temperature = WeatherController.Singleton.Temperature;
+    protected void IncreaseTemperaturePerSecond(float amount) => temperature += amount * Time.deltaTime;
+    protected void DecreaseTemperaturePerSecond(float amount) => temperature -= amount * Time.deltaTime;
+    protected void HeatUp() => IncreaseTemperaturePerSecond(2);
+    protected void CheckAsGoneIntoStandby() => GoneIntoStandby = true;
+    protected void UncheckAsGoneIntoStandby() => GoneIntoStandby = false;
+    protected void CoolDown() => DecreaseTemperaturePerSecond(1);
     protected void EPO ()
     {
         if(MachineAboveSafeTemperature)
         {
-            TurnOffMachine();
+            TurnOff();
         }
     }
 
-    protected void CoolDown ()
-    {
-        DecreaseTemperaturePerSecond(1);
-    }
-
-    protected void DecreaseTemperaturePerSecond (float amount)
-    {
-        temperature -= amount * Time.deltaTime;
-    }
+   
 
     protected void ThisStart ()
     {
         SetTemperatureToAmbient();
 
-        TurnOnMachine();
+        TurnOn();
     }
 
     protected void ThisUpdate ()
     {
-        if (powerState == MachineState.On)
+        temperatureDisplay.text = temperature.ToString("0.0") + "C";
+
+        powerStateDisplay.text = powerState.ToString();
+
+
+        if(GameManager.Singleton.IsInBuildPhase && !GoneIntoStandby)
+        {
+            if(MachineIsOn)
+            {
+                CheckAsGoneIntoStandby();
+
+                GoIntoStandby();
+            }
+        }
+
+        if(GameManager.Singleton.IsInAssaultPhase)
+        {
+            if(MachineIsInStandby)
+            {
+
+            }
+        }
+
+        if (MachineIsOn)
         {
             HeatUp();
         }
 
-        if(powerState == MachineState.Off)
+        if(MachineIsOff)
         {
             CoolDown();
         }
